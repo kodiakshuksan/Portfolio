23b1d2d7042ba2d4fc17aff72a38b3b20ffb7dd5
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-05-14 16:34:14 -0700

    first commit created app and components

---
+++ my_new_portfolio/portfolio/src/App.js
@@ -0,0 +1,23 @@
+import About from './Components/About/About';
+import Body from './Components/Body/Body';
+import Home from './Components/Home/Home';
+import Footer from './Components/Footer/Footer';
+import NavBar from './Components/NavBar/NavBar';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+      Hello
+      <NavBar/>
+      <Home/>
+      <Body/>
+      <About/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my_new_portfolio/portfolio/src/Components/Body/Body.js
@@ -0,0 +1,12 @@
+const body = () => {
+    return (
+        <div>
+            Body
+        </div>
+    )
+}
+
+export default body;
+
+
+
