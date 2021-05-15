2c1cdd45d0f6ea321eedfcfec151801f011b91b1
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-05-14 20:35:33 -0700

    added css, body, navbar, footer

--- my_new_portfolio/portfolio/src/Components/Body/Body.js
+++ my_new_portfolio/portfolio/src/Components/Body/Body.js
@@ -1,7 +1,28 @@
+const styles = {
+header: {
+    background: "#b71775",
+    height: "609px"
+    
+  },
+  
+  headerH1: {
+    margin: "0",
+    padding_top: "2000px",
+    text_align: "center",
+    color: "white",
+    font_size: "3rem"
+  }
+};
+  
+
+
+
+
 const body = () => {
     return (
-        <div>
-            Body
+        <div style={styles.header}>
+            
+            
         </div>
     )
 }
--- my_new_portfolio/portfolio/src/Components/Footer/Footer.js
+++ my_new_portfolio/portfolio/src/Components/Footer/Footer.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 const footer = () => {
     return (
+        
         <div>
-            Footer
+            {<Navbar bg="light" expand="lg">
+        
+                <Nav className="mr-auto">
+                    
+                    <Nav.Link href="#GitHub">GitHub</Nav.Link>
+                    <br></br>
+                    <Nav.Link href="#Linkedin">Linkedin</Nav.Link>
+                   
+                </Nav>
+                </Navbar>}
         </div>
+                
+        
     )
 }
 
-export default footer
+export default footer;
