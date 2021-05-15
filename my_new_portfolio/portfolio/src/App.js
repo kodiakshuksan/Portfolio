5c829c5f1d511b5f22cd08116a251d313c2ac25b
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-05-14 18:00:01 -0700

    added files, components

--- my_new_portfolio/portfolio/src/App.js
+++ my_new_portfolio/portfolio/src/App.js
@@ -3,18 +3,23 @@ import Body from './Components/Body/Body';
 import Home from './Components/Home/Home';
 import Footer from './Components/Footer/Footer';
 import NavBar from './Components/NavBar/NavBar';
+import Resume from './Components/Resume/Resume';
+import Projects from './Components/Projects/Projects';
+//import Navbar from 'react-bootstrap/Navbar';
+
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
-      Hello
       <NavBar/>
       <Home/>
       <Body/>
       <About/>
+      <Resume/>
+      <Projects/>
       <Footer/>
     </div>
   );
